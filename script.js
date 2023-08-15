0aeadac023404672120ffe15206ebb10fc713b2c
Luke Wheldale <lukewheldale@gmail.com>
2023-08-15 20:22:06 +0930

    add basic js to page

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * This is a function
+ * @param {*} someArgument - Some data given to the function
+ */
+
+function someFunction(someArgument) {
+	console.log("This was given to the function:" + someArgument);
+}
+
+someFunction([1, 2, "banana"]);
